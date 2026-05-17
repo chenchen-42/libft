7abc9e8716fdd82d890ecec24e2a0708c0b13c4b
chenchen-42 <andremchen@gmail.com>
2026-05-17 02:10:36 +0100

    update

--- ft_calloc.c
+++ ft_calloc.c
@@ -17,7 +17,7 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	void	*ptr;
 
 	if (nmemb == 0 || size == 0)
-    	return (malloc(0));
+    	return (NULL);
 	if (size != 0 && size > SIZE_MAX / nmemb)
 		return (NULL);
 	ptr = malloc(nmemb * size);
